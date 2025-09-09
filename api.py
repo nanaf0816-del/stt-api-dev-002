fadacdb425824d8fd501062c5a7504216eac4793
nanaf0816-del <nanaf.0816@gmail.com>
2025-09-09 17:51:15 +0900

    ２回目のコミット

--- api.py
+++ api.py
@@ -84,4 +84,4 @@ async def generate_next_question(request: AnswerRequest):
 
 # このスクリプトを直接実行した場合に、uvicornサーバーを起動
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
